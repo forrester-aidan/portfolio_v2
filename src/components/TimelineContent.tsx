33f8eb2792971095b70531a55558737bd5ad3fae
forrester-aidan <forrester.aidan@ufl.edu>
2024-12-22 14:14:54 -0500

    fix: css styling background

--- src/components/TimelineContent.tsx
+++ src/components/TimelineContent.tsx
@@ -12,10 +12,10 @@ const TimelineContent = () => {
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
           contentStyle={{
-            background: 'rgb(33, 150, 243)',
+            background: 'rgb(55, 65, 81)',
             color: '#fff',
           }}
-          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          contentArrowStyle={{ borderRight: '7px solid rgb(55, 65, 81)' }}
         >
           <h3 className="vertical-timeline-element-title">
             Research Assistant
@@ -24,22 +24,24 @@ const TimelineContent = () => {
             Cornell Tech - Roosevelt Island, NYC
           </h4>
           <p>Arduino, C programming, Teaching HTML/CSS</p>
+          <p className="text-red-300">May 2024 - August 2024</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          contentStyle={{ background: 'rgb(55, 65, 81)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(55, 65, 81)' }}
         >
           <h3 className="vertical-timeline-element-title">Web Developer</h3>
           <h4 className="vertical-timeline-element-subtitle">
             Society for Asian Scientists and Engineers
           </h4>
           <p>TypeScript, React, Tailwind CSS, Node.js, Git</p>
+          <p className="text-red-300">January 2024 - December 2024</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          contentStyle={{ background: 'rgb(55, 65, 81)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(55, 65, 81)' }}
         >
           <h3 className="vertical-timeline-element-title">
             Officer Training Course
@@ -48,11 +50,12 @@ const TimelineContent = () => {
             Software Engineering Club
           </h4>
           <p>HTML/CSS, JavaScript, Node.js/Express.js, MongoDB, React</p>
+          <p className="text-red-300">January 2023 - May 2023</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          contentStyle={{ background: 'rgb(55, 65, 81)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid rgb(55, 65, 81)' }}
         >
           <h3 className="vertical-timeline-element-title">
             Open Source Project - Swamp Scheduler
@@ -61,14 +64,15 @@ const TimelineContent = () => {
             Open Source Club
           </h4>
           <p>TypeScript, React, Tailwind CSS, Node.js, Git</p>
+          <p className="text-red-300">September 2022 - November 2022</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
           contentStyle={{
-            background: 'rgb(33, 150, 243)',
+            background: 'rgb(55, 65, 81)',
             color: '#fff',
           }}
-          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          contentArrowStyle={{ borderRight: '7px solid rgb(55, 65, 81)' }}
         >
           <h3 className="vertical-timeline-element-title">
             Computer Science - University of Florida
@@ -77,11 +81,12 @@ const TimelineContent = () => {
             Bachelor's Degree
           </h4>
           <p>Python, C++, ARM, R</p>
+          <p className="text-red-300">August 2022 - Present</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          contentStyle={{ background: 'rgb(55, 65, 81)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid rgb(55, 65, 81)' }}
         >
           <h3 className="vertical-timeline-element-title">
             Vocational Certificate in Information Technology
@@ -90,6 +95,7 @@ const TimelineContent = () => {
             High School Diploma
           </h4>
           <p>HTML/CSS, JavaScript, Java, SQL</p>
+          <p className="text-red-300">September 2018 - June 2022</p>
         </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
